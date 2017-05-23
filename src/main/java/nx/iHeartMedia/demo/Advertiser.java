01e4b805b2ebdd13a0b34d2b1e5a91b71608606e
Nicholas Yearwood <nx.iheartmedia@guerrillamail.org>
2017-05-23 18:18:17 -0500

    - initial commit

---
+++ src/main/java/nx/iHeartMedia/demo/Advertiser.java
@@ -0,0 +1,8 @@
+package nx.iHeartMedia.demo;
+
+/**
+ * Created by Nx on 5/23/2017.
+ */
+public class Advertiser {
+    
+}
---
+++ src/main/java/nx/iHeartMedia/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package nx.iHeartMedia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.annotations.ApiOperation;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class})
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+	}
+}
