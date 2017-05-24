aa9ed9f0b77f3cce7664cf4e92e8a761c4f4acaf
Nicholas Yearwood <nx.iheartmedia@guerrillamail.org>
2017-05-24 10:53:40 -0500

    - fixed dependencies

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Nx.iHeartMedia.Demo
 ### About
 Author: `Nicholas Yearwood` 2017.05.24
-A simple demo application using Java 8, Spring, Spring Boot, Swagger and more
+A simple demo application using Java 8, Spring, Spring Boot, Swagger and more.
 ### Building
 Import as Gradle project into IntelliJ IDEA and select `Build // Build Artifacts...`. The output will be in `<source>/demo/classes/artifacts/demo_jar/demo.jar`.
 Alternatively, from a command prompt properly configured for Java 8/Gradle builds, execute the following command from the `<source>/demo` directory: `gradle demoJar`. Using this option, the output Jar will be in `<source>/build/libs`.
--- src/main/java/nx/iHeartMedia/demo/Advertiser.java
+++ src/main/java/nx/iHeartMedia/demo/Advertiser.java
@@ -2,17 +2,26 @@ package nx.iHeartMedia.demo;
 
 import java.util.UUID;
 import javax.persistence.*;
+
 /**
  * Created by Nx on 5/23/2017.
  */
+@Entity
 public class Advertiser {
- //   @Id
- //   @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @Id
+    @GeneratedValue
+    public UUID id;
 
-    private String Name;
+    public String name;
 
-    private String PrimaryContact;
+    public String primary_contact;
 
-    private double CreditLimit;
+    public double credit_limit;
+
+    public Advertiser(UUID id, String name, String primaryContact, double creditLimit) {
+        this.id = id;
+        this.name = name;
+        this.primary_contact = primaryContact;
+        this.credit_limit = creditLimit;
+    }
 }
